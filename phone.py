78a350b7203d1e10132ceb938dd1362dcf7c5bfb
uddhavchavan <153163033+uddhavchavan@users.noreply.github.com>
2024-01-10 13:01:32 +0530

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,33 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+import opencage
+from opencage.geocoder import OpenCageGeocode
+import folium
+
+
+key = "b32d5a4bb43647f49c9eed0731e69bc4" #Geocoder API Key need to paste here "your key"
+number = input("please giver your number: ")
+new_number = phonenumbers.parse(number)
+location = geocoder.description_for_number(new_number, "en")
+print(location)
+
+service_name = carrier.name_for_number(new_number,"en")
+print(service_name)
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+result = geocoder.geocode(query)
+#print(result)
+
+lat = result[0]['geometry']['lat']
+lng = result[0]['geometry']['lng']
+
+print(lat,lng)
+
+my_map = folium.Map(location=[lat,lng], zoom_start=9)
+folium.Marker([lat, lng], popup= location).add_to(my_map)
+
+my_map.save("4.html")
+
+print("location tracking completed")
